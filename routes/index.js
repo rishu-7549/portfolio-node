79632c221f972042d4e1b82c3e8574d6523a9ce1
Rishu <rishu02k@gmail.com>
2025-07-23 02:06:01 +0530

    setup dotenv

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,13 @@
 var express = require('express');
+require('dotenv').config();
 var router = express.Router();
 var mysql = require('mysql');
 
 var connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'portfolio'
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
 });
 
 connection.connect();
